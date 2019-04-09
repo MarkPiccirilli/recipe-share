4105ad6759543c88bfae1bebb75270be4d697531
Mark Piccirilli <mpicc704@gmail.com>
2019-04-08 20:13:00 -0400

    Search and filter functionality improved

--- searchResults.js
+++ searchResults.js
@@ -3,16 +3,47 @@ module.exports= function(){
     var router = express.Router();
 
     function getRecipesByName(req, res, mysql, context, complete) {
-        var query = "SELECT id, name FROM recipes where name=?";
-        console.log("input: " + req.body.keyword);
-        var inserts = [req.body.keyword];
+        var query;
+        var inserts;
+        console.log("keyword: " + req.body.keyword);
+        console.log("meal_type: " + req.body.meal_type);
+        console.log("ethnic_cuisine: " + req.body.ethnic_cuisine);
+        if(req.body.meal_type == "" && req.body.ethnic_cuisine == "" && req.body.keyword == "") {
+
+        }
+        else if(req.body.meal_type == "" && req.body.ethnic_cuisine == "") {
+            query = "SELECT id, name FROM recipes WHERE name=?;";
+            inserts = [req.body.keyword];
+            console.log("nametest");
+        }
+        else if(req.body.meal_type == "" && req.body.keyword == "") {
+            query = "SELECT id, name FROM recipes WHERE ethnic_cuisine=?;";
+            inserts = [req.body.ethnic_cuisine];
+        }
+        else if(req.body.ethnic_cuisine == "" && req.body.keyword == "") {
+            query = "SELECT id, name FROM recipes WHERE meal_type=?;";
+            inserts = [req.body.meal_type];
+        }
+        else if(req.body.keyword == "") {
+            query = "SELECT id, name FROM recipes WHERE meal_type=? AND ethnic_cuisine=?;";
+            inserts = [req.body.meal_type, req.body.ethnic_cuisine];
+        }
+        else if(req.body.ethnic_cuisine == "") {
+            console.log("namemealtest");
+            query = "SELECT id, name FROM recipes WHERE name=? and meal_type=?";
+            inserts = [req.body.keyword, req.body.meal_type];
+        }
+        else {
+            console.log("namemealethnictest");
+            query = "SELECT id, name FROM recipes WHERE name=? AND meal_type=? AND ethnic_cuisine=?;";
+            inserts = [req.body.keyword, req.body.meal_type, req.body.ethnic_cuisine];
+        }
         mysql.pool.query(query, inserts, function(error, results, fields) {
             if(error) {
                 res.write(JSON.stringify(error));
                 res.end();
             }
             context.recipes = results;
-	        console.log("function: " + JSON.stringify(context));
             complete();
         });
     }
@@ -31,7 +62,5 @@ module.exports= function(){
         }
     });
 
-
-    //router.get('/filter_search/
     return router;
 }();
